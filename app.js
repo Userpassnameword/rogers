4f64148516ae22d004dabbf151223823946492b0
Userpassnameword <144236256+Userpassnameword@users.noreply.github.com>
2023-09-06 07:53:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_6d1qs16", "template_a5nnm4d", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
